0fff9d2bad8445cf026dfb66ab2b70f4e9b190d7
jlistinsky <jslistinsky@gmail.com>
2021-11-29 20:03:33 -0800

    echo added

---
+++ echo.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "Echo.h"
+
+
+Echo::Echo(int delay): delay(delay) 
+{
+    if((delay % 2) != 0)
+    {
+        delay = delay + 1;
+    }
+}
+
+
+void Echo::processBuffer(unsigned char* buffer, int bufferSize, wav wavfile){
+    if(wavfile.getFMT().bit_depth == 8)
+    {
+    for(int i=delay;i<bufferSize;i++)
+        {
+        buffer[i] = buffer[i] + buffer[i-delay]; 
+        }
+    }
+    else if (wavfile.getFMT().bit_depth == 16)
+    {
+    for(int i=delay;i<bufferSize/2;i++)
+        {
+        ((short*)buffer)[i] = ((short*)buffer)[i] + ((short*)buffer)[i-delay];
+        }
+    }
+}
